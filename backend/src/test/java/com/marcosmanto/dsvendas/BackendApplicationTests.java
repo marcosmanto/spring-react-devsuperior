b391ea521148fe6139e15d4a962754d468aeefe8
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-09-08 17:37:59 -0300

    project scaffold

---
+++ backend/src/test/java/com/marcosmanto/dsvendas/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosmanto.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
